6e2b59ace5f48615bef0cb3d88edfe4dca4fbc9a
Facundo Martin Gordillo <me@facundogordillo.com>
2022-11-22 18:57:09 +0100

    feat: added essential code (#1)
    * feat: added `tailwind` + removed unused code
    
    * feat: removed unused `/api` folder + added missing files
    
    * feat: added missing files

---
+++ containers/Home.tsx
@@ -0,0 +1,11 @@
+import type { FunctionComponent } from "react";
+
+const HomeContainer: FunctionComponent = () => {
+  return (
+    <main className="flex h-screen justify-center items-center">
+      Hello world
+    </main>
+  );
+};
+
+export default HomeContainer;
